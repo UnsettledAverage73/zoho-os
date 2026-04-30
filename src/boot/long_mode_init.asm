652661e2a77460ab33e11e5b7887a2096c937328
unsettledaverage73 <atharvabodade@gmail.com>
2026-05-01 01:25:04 +0530

    feat: add 64-bit kernel entry assembly

---
+++ src/boot/long_mode_init.asm
@@ -0,0 +1,18 @@
+global long_mode_start
+extern kmain
+
+section .text
+bits 64
+long_mode_start:
+    ; rdi contains multiboot info pointer
+    
+    ; load 0 into all data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    call kmain
+    hlt
