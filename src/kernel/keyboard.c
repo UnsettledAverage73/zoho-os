304b0dc02e85d61b7a9ada88a91879110f0a3440
unsettledaverage73 <atharvabodade@gmail.com>
2026-05-01 01:26:35 +0530

    feat: implement PS/2 keyboard driver

---
+++ src/kernel/keyboard.c
@@ -0,0 +1,36 @@
+#include "keyboard.h"
+#include "vga.h"
+#include "serial.h"
+
+static uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    __asm__ volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
+    return ret;
+}
+
+static const char kbd_us[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\',
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '-', 0, 0, 0, '+', 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+extern void shell_input(char c);
+
+void keyboard_handler() {
+    uint8_t scancode = inb(0x60);
+    if (scancode & 0x80) {
+        // Key release
+    } else {
+        char c = kbd_us[scancode];
+        if (c) {
+            shell_input(c);
+        }
+    }
+}
+
+void keyboard_init() {
+    // PIC is already remapped, IRQ1 is INT 33
+}
