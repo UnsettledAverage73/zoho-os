c9d968ffd8ead68a96fe12a3bc27975ccb0190cd
unsettledaverage73 <atharvabodade@gmail.com>
2026-05-11 04:05:08 +0530

    Update graphics.h header

--- include/kernel/graphics.h
+++ include/kernel/graphics.h
@@ -18,10 +18,12 @@ void graphics_put_pixel(uint32_t x, uint32_t y, uint32_t color);
 void graphics_clear(uint32_t color);
 void graphics_clear_rect(int32_t x, int32_t y, int32_t w, int32_t h, uint32_t color);
 void graphics_draw_rect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color);
+void graphics_draw_line_buffer(uint32_t x, uint32_t y, uint32_t w, uint32_t* buffer);
 void graphics_draw_cursor(uint32_t x, uint32_t y);
 void graphics_draw_char(uint32_t* buf, uint32_t w, uint32_t x, uint32_t y, char c, uint32_t color);
 void graphics_draw_string(uint32_t* buf, uint32_t w, uint32_t x, uint32_t y, const char* str, uint32_t color);
 uint32_t graphics_get_width();
 uint32_t graphics_get_height();
+uint64_t graphics_get_fb_addr();
 
 #endif
