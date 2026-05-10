1e3916095824f2e7c0e64a1183353953e837faa3
unsettledaverage73 <atharvabodade@gmail.com>
2026-05-11 03:59:27 +0530

    Update kmalloc.h header

--- include/kernel/kmalloc.h
+++ include/kernel/kmalloc.h
@@ -4,8 +4,20 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef struct {
+    size_t alloc_count;
+    size_t free_count;
+    size_t live_allocations;
+    size_t bytes_used;
+    size_t peak_bytes_used;
+    size_t bytes_free;
+    size_t largest_free_block;
+    size_t failed_allocations;
+} kmalloc_stats_t;
+
 void kmalloc_init();
 void* kmalloc(size_t size);
 void kfree(void* ptr);
+void kmalloc_get_stats(kmalloc_stats_t* out_stats);
 
 #endif
