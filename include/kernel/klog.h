d08956dc88b04f68ba231a046809822395ab5792
unsettledaverage73 <atharvabodade@gmail.com>
2026-05-11 03:58:23 +0530

    Update klog.h header

--- include/kernel/klog.h
+++ include/kernel/klog.h
@@ -1,6 +1,9 @@
 #ifndef KLOG_H
 #define KLOG_H
 
+#include <stdarg.h>
+#include <stddef.h>
+
 typedef enum {
     LOG_DEBUG,
     LOG_INFO,
@@ -8,6 +11,14 @@ typedef enum {
     LOG_ERROR
 } log_level_t;
 
-void klog(log_level_t level, const char* module, const char* fmt);
+#ifndef KLOG_MIN_LEVEL
+#define KLOG_MIN_LEVEL LOG_WARN
+#endif
+
+void klog(log_level_t level, const char* module, const char* fmt, ...);
+void klog_set_level(log_level_t level);
+log_level_t klog_get_level();
+
+int vsnprintf(char* str, size_t size, const char* format, va_list ap);
 
 #endif
