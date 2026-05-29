c5baf451381c26f738f76105c6a7b8addda40d41
unsettledaverage73 <atharvabodade@gmail.com>
2026-05-30 00:34:46 +0530

    feat(userland): implement minimal shell application and linker script

---
+++ src/apps/shell/main.c
@@ -0,0 +1,1 @@
+#include <stdint.h> void _start() { const char* msg = "Zoho OS User Shell v1.0\nzoho> "; uint32_t len = 0; while (msg[len]) len++; __asm__ volatile ("mov $6, %%rax\n" "mov $1, %%rdi\n" "mov %0, %%rsi\n" "mov %1, %%rdx\n" "syscall" : : "r"(msg), "r"((uint64_t)len) : "rax", "rdi", "rsi", "rdx", "rcx", "r11"); while (1) { __asm__ volatile ("pause"); } }
